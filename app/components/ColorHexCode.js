7d694c78ceeaa314a336d651a8f55ba7b0bf03ce
Yoshihide Jimbo <yjimbo@gmail.com>
2015-10-16 23:20:44 +0900

    Enable to copy hex code and color name

--- app/components/ColorHexCode.js
+++ app/components/ColorHexCode.js
@@ -1,12 +1,21 @@
 'use strict';
 
 var React = require('react');
+var clipboard = require('clipboard');
 
 var ColorHexCode = React.createClass({
+  handleClick: function(e) {
+    var target = e.target;
+    setTimeout(function() {
+      clipboard.writeText(target.textContent);
+    }, 0);
+  },
+
   render: function () {
+    var value = this.props.hex;
     return (
-      <div className="color-hex-code">
-        {this.props.hex}
+      <div className="color-hex-code" onClick={this.handleClick}>
+        {value}
       </div>
     )
   }
--- app/components/ColorName.js
+++ app/components/ColorName.js
@@ -1,12 +1,21 @@
 'use strict';
 
 var React = require('react');
+var clipboard = require('clipboard');
 
 var ColorName = React.createClass({
+  handleClick: function(e) {
+    var target = e.target;
+    setTimeout(function() {
+      clipboard.writeText(target.textContent);
+    }, 0);
+  },
+
   render: function () {
+    var value = this.props.name;
     return (
-      <div className="color-name">
-        {this.props.name}
+      <div className="color-name" onClick={this.handleClick}>
+        {value}
       </div>
     )
   }
